4b0ace927a4b3e75d466c31bb99c5ae3bb9d7ee8
cbochs <calvin.bochulak@gmail.com>
2021-02-16 20:49:21 -0700

    Fix linting

--- statify/config.py
+++ statify/config.py
@@ -3,5 +3,8 @@ from pathlib import Path
 
 VERSION = '1.0'
 
-STATIFY_PATH = Path(os.environ.get('STATIFY_DATA', Path.home() / '.data' / 'statify'))
-CONFIG_PATH  = Path(os.environ.get('STATIFY_CONFIG', Path.home() / '.config' / 'statify.yaml'))
+STATIFY_PATH = Path(os.environ.get('STATIFY_DATA', 
+                    Path.home() / '.data' / 'statify'))
+
+CONFIG_PATH = Path(os.environ.get('STATIFY_CONFIG',
+                    Path.home() / '.config' / 'statify.yaml'))
