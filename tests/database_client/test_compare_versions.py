08eb7df70fb75ebebdd75fcf3bbd62e3f560a14d
Valentin <>
2021-05-01 21:04:30 +0200

    Improve database upgrade system to remove the need to append each new version

--- tests/database_client/test_compare_versions.py
+++ tests/database_client/test_compare_versions.py
@@ -29,3 +29,14 @@ def test_init_from_database_from_up_version(statify_directory):
 
     with pytest.raises(database_client.DowngradeVersionError):
         database_client.StatifyDatabase(':memory')
+
+
+def test_init_database_without_any_additional_sql(statify_directory, sql_spy, mocker):
+    with open(str(database_client.DATABASE_VERSION_PATH), 'w') as version_file:
+        version_file.write('1.1.0')
+
+    mocker.patch.object(config, 'VERSION', '1.2.0')
+
+    database_client.StatifyDatabase(':memory')
+
+    assert sql_spy.mock_calls == []
